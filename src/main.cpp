f38ce5e2b9d8b6a866cb17b711c2f00293384962
Twometer <twometer@outlook.de>
2022-01-31 15:33:44 +0100

    Create simple project

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_SAMPLES, 8);
+    auto window = glfwCreateWindow(1024, 768, "Blink Editor", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cerr << "Failed to load OpenGL" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glClearColor(26.0 / 255.0, 27.0 / 255.0, 38.0 / 255.0, 1.0);
+
+    while (!glfwWindowShouldClose(window)) {
+        int w, h;
+        glfwGetFramebufferSize(window, &w, &h);
+
+        glViewport(0, 0, w, h);
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    return 0;
+}
