948069d69be1376282addb7635b1005683ca2814
Twometer <twometer@outlook.de>
2022-01-31 16:44:28 +0100

    Smoother draw

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,16 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void redraw(GLFWwindow *window) {
+    int w, h;
+    glfwGetFramebufferSize(window, &w, &h);
+
+    glViewport(0, 0, w, h);
+    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+}
+
 int main() {
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
@@ -13,6 +23,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_SAMPLES, 8);
+
     auto window = glfwCreateWindow(1024, 768, "Blink Editor", nullptr, nullptr);
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
@@ -23,18 +34,15 @@ int main() {
         return 1;
     }
 
+    glfwSetWindowRefreshCallback(window, redraw);
+
     glClearColor(26.0 / 255.0, 27.0 / 255.0, 38.0 / 255.0, 1.0);
-
     while (!glfwWindowShouldClose(window)) {
-        int w, h;
-        glfwGetFramebufferSize(window, &w, &h);
-
-        glViewport(0, 0, w, h);
-        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
+        redraw(window);
         glfwPollEvents();
     }
 
     return 0;
 }
+
+
